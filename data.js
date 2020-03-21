9aab33b62786859b0ee5dca3f9103f0194d60045
dhondtsimon <simodhon2@student.arteveldehs.be>
2020-03-21 19:53:30 +0100

    Opdracht 2 ingediend

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/69db02b3-039f-4149-6c5f-f06d508df1fd-1fea/grid",
                 "target": "_blank"
             }
         ]
